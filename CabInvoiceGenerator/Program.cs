5c4454a721deabf45af36eae253f148b372abe33
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-04-17 17:06:05 +0530

    [Ganesh] Add:Cab Invoice Generator file is added

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CabInvoiceGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wecome to cab invoice generator");
+        }
+    }
+}
